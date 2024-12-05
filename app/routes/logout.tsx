b614d7a14d672256ae554d37c731eae62c2e5154
Luca Di Bello <lucadibello@proton.me>
2024-12-05 18:25:42 +0100

    Added disclaimer

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,6 +1,12 @@
 import { ActionFunctionArgs, redirect } from "@remix-run/node";
 import { destroySession, getSession } from "~/lib/session";
 
+//
+// FIXME: This should not be done as it would open users to
+// Cross-Site Request Forgery (CSRF) attacks.
+//
+// Read documentation: https://remix.run/docs/ja/main/utils/sessions#createcookiesessionstorage
+
 export async function loader({ request }: ActionFunctionArgs) {
   // Get session and destroy it (if exists)
   const session = await getSession(request.headers.get("Cookie"));
